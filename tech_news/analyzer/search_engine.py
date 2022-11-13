a6ea83954fa20e67a4ea26f875375c25f59dc34e
annatrajano <annatrajano95@gmail.com>
2022-11-12 21:43:39 -0300

    feat: :fire: add req08 and req09  -  create def search_by_tag and def search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -4,11 +4,11 @@ from tech_news.database import search_news
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    news_list = search_news({"title": {"$regex": title, "$options": "-i"}})
-    titles = []
-    for news in news_list:
-        titles.append((news["title"], news["url"]))
-    return titles
+    query = search_news({"title": {"$regex": title, "$options": "-i"}})
+    results = []
+    for news in query:
+        results.append((news["title"], news["url"]))
+    return results
 
 
 # Requisito 7
@@ -19,8 +19,20 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     """Seu código deve vir aqui"""
+    query = search_news({"tags": {"$regex": tag, "$options": "-i"}})
+    results = []
+    for news in query:
+        results.append((news["title"], news["url"]))
+    return results
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    query = search_news(
+        {"category": {"$regex": category, "$options": "-i"}}
+    )
+    results = []
+    for news in query:
+        results.append((news["title"], news["url"]))
+    return results
